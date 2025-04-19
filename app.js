144adf7ffd6340f9733a96d02baa6bacdba41128
otong-dev <muhamadyuaiffadilah567@gmail.com>
2025-04-19 16:27:09 +0700

    sorce code dasar

--- app.js
+++ app.js
@@ -4,17 +4,15 @@ const rl = readline.createInterface({
     input : process.stdin,
     output : process.stdout,
 });
-
 rl.question('masukan nama anda : ', (nama) => {
     rl.question('masukan umur anda : ', (umur) => {
-        const data = { nama , umur};
-        console.log(data);
+        const contact = {nama, umur};
         const file = fs.readFileSync('data/contacts.json', 'utf8');
-        const datas = JSON.parse(file);
-        console.log(datas)
-        datas.push(data);
-        console.log('terima kasih telah masukan data');
-        fs.writeFileSync('data/contacts.json', JSON.stringify(datas));
-        rl.close()
-    });
-});
+        const contacts = JSON.parse(file);
+        contacts.push(contact);
+        console.log(`halo ${nama} kamu berumur ${umur} tahun`);
+        fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+        console.log(contacts);
+        rl.close();
+    })
+})
