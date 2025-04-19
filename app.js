cbc65ec9868f9d1fbc0ae727917bfb7cbc648248
otong-dev <muhamadyuaiffadilah567@gmail.com>
2025-04-19 15:52:07 +0700

    source code awal

--- app.js
+++ app.js
@@ -1,36 +1,20 @@
 const fs = require('fs');
 const readline = require('readline');
-
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+    input : process.stdin,
+    output : process.stdout,
 });
 
-// membuat file data
-const dirPath = './data'
-if(!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath);
-}
-
-// membuat file contacts.json jika belum ada
-const dataPath = './data/contacts.json';
-if(!fs.existsSync(dataPath)){
-    fs.writeFileSync(dataPath, '[]', 'utf-8');
-}
-
-rl.question('masukan nama anda : ' , (nama) => {
-    rl.question('masukan no Hp anda : ' , (noHP) => {
-        rl.question('masukan email anda : ' , (email) => {
-        const contact = {nama, noHP};
-        const fileBuffer = fs.readFileSync('data/contacts.json','utf-8');
-        const contacts = JSON.parse(fileBuffer);
-
-        contacts.push(contact);
-
-        fs.writeFileSync('data/contacts.json',JSON.stringify(contacts));
-
-        crossOriginIsolated.log('terima kasih sudah masukan data.')
-        rl.close();
-        });
+rl.question('masukan nama anda : ', (nama) => {
+    rl.question('masukan umur anda : ', (umur) => {
+        const data = { nama , umur};
+        console.log(data);
+        const file = fs.readFileSync('data/contacts.json', 'utf8');
+        const datas = JSON.parse(file);
+        console.log(datas)
+        datas.push(data);
+        console.log('terima kasih telah masukan data');
+        fs.writeFileSync('data/contacts.json', JSON.stringify(datas));
+        rl.close()
     });
-})
+});
