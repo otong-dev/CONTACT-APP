aa4f9807cbdbf13a3c201cd783c6341682350439
otong-dev <muhamadyuaiffadilah567@gmail.com>
2025-04-20 07:39:56 +0700

    sebelum di refactoring

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const rl = readline.createInterface({
     output : process.stdout,
 });
 rl.question('masukan nama anda : ', (nama) => {
-    rl.question('masukan umur anda : ', (umur) => {
-        const contact = {nama, umur};
+    rl.question('masukan noHP anda : ', (noHP) => {
+        const contact = {nama, noHP};
         const file = fs.readFileSync('data/contacts.json', 'utf8');
         const contacts = JSON.parse(file);
         contacts.push(contact);
-        console.log(`halo ${nama} kamu berumur ${umur} tahun`);
+        console.log(`halo ${nama} noHP kamu ${noHP}`);
         fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
         console.log(contacts);
         rl.close();
