ff14c0bb960cce59f3ded49fe67bc389f769d12a
otong-dev <muhamadyuaiffadilah567@gmail.com>
2025-04-24 12:04:33 +0700

    mulai

--- app.js
+++ app.js
@@ -1,45 +1,9 @@
-const fs = require('fs');
-const readline = require('readline');
-const rl = readline.createInterface({
-    input : process.stdin,
-    output : process.stdout,
-});
-
-// membuat folder data jika belum ada
-const dirPath = './data'
-if(!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath);
-}
-
-// membuat file contacts.json jika belum ada 
-const dataPath = './data/contacts.json';
-if(!fs.existsSync(dataPath)) {
-    fs.writeFileSync(dataPath, '[]', 'utf-8')
-}
-
-
-const pertanyaan1 = () => {
-    return new Promise((resolve, reject) => {
-        rl.question('masukan nama anda : ', (nama) => {
-            resolve(nama);
-        });
-    });
-};
-
-const pertanyaan2 = () => {
-    return new Promise((resolve, reject) => {
-        rl.question('masukan email anda : ', (email) => {
-            resolve(email);
-        });
-    });
-};
-
-
 const main = async() => {
-    const nama = await pertanyaan1();
-    const email = await pertanyaan2();
+    const nama = await tulisPertanyaan("masukan nama anda :");
+    const email = await tulisPertanyaan("masukan email anda :");
+    const noHP= await tulisPertanyaan("masukan noHP anda :");
 
-    const contact = {nama, email};   
+    const contact = {nama, email, noHP};   
     const file = fs.readFileSync('data/contacts.json', 'utf8');
     const contacts = JSON.parse(file);
     contacts.push(contact);
@@ -50,21 +14,3 @@ const main = async() => {
 };
 
 main();
-
-
-
-
-
-
-// rl.question('masukan nama anda : ', (nama) => {
-//     rl.question('masukan noHP anda : ', (noHP) => {
-//         const contact = {nama, noHP};
-//         const file = fs.readFileSync('data/contacts.json', 'utf8');
-//         const contacts = JSON.parse(file);
-//         contacts.push(contact);
-//         console.log(`halo ${nama} noHP kamu ${noHP}`);
-//         fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
-//         console.log(contacts);
-//         rl.close();
-//     });
-// });
